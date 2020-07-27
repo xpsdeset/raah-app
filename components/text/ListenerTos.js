5be548643f6d3514324d1ec1959c31452cf226e1
Sethu <Sethu>
2020-07-27 19:27:38 +0530

    add new tos

--- components/text/ListenerTos.js
+++ components/text/ListenerTos.js
@@ -8,14 +8,14 @@ let str = `
 <li>Be present</li>
 <li>Give attention</li>
 </ul>
-<li>&nbsp;Keep an open mind.Listen without judging the other person or mentally criticizing the things she tells you. If what she says alarms you, go ahead and feel alarmed, but don't say to yourself, "Well, that was a stupid move." As soon as you indulge in judgmental amusements, you've compromised your effectiveness as a listener.</li>
+<li>&nbsp;Keep an open mind.Listen without judging the other person or mentally criticizing the things he/she tells you. If what he/she says alarms you, go ahead and feel alarmed, but don't say to yourself, "Well, that was a stupid move." As soon as you indulge in judgmental amusements, you've compromised your effectiveness as a listener.</li>
 <li>Listen to the words and try to picture what the speaker is saying.</li>
 <li>Allow your mind to create a mental model of the information being communicated. Whether a literal picture, or an arrangement of abstract concepts, your brain will do the necessary work if you stay focused, with senses fully alert. When listening for long stretches, concentrate on, and remember, key words and phrases.</li>
 <li>Don't interrupt and don't impose your "solutions."</li>
 <li>Wait for the speaker to pause to ask clarifying questions.</li>
 <li>Ask questions only to ensure understanding.</li>
 <li>Try to feel what the speaker is feeling.</li>
-<li>If you feel sad when the person with whom you are talking expresses sadness, joyful when she expresses joy, fearful when she describes her fears&mdash;and conveys those feelings through your facial expressions and words&mdash;then your effectiveness as a listener is assured. Empathy is the heart and soul of good listening.</li>
+<li>If you feel sad when the person with whom you are talking expresses sadness, joyful when he/she expresses joy, fearful when he/she describes her fears&mdash;and conveys those feelings through your facial expressions and words&mdash;then your effectiveness as a listener is assured. Empathy is the heart and soul of good listening.</li>
 </ol>
 </div>
 `
